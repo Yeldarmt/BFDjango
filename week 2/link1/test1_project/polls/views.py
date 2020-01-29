c85ca32a62216d09a95bc06f40b3c1bbc5e221a4
Eldar <eldarmukhametkazin@gmail.com>
2020-01-30 02:08:45 +0600

    first app

---
+++ week 2/link1/test1_project/polls/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def about(request):
+    return HttpResponse("About page")
+
+
+def time(request, pk):
+    return HttpResponse('<h1>int pk = {}</h2>'.format(pk))
+
+
