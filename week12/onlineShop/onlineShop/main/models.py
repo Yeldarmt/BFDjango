2fc850e7afdbc69d940307b144521556203ca076
Eldar <eldarmukhametkazin@gmail.com>
2020-04-17 00:30:46 +0600

    week12 updated

--- week12/onlineShop/onlineShop/main/models.py
+++ week12/onlineShop/onlineShop/main/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+import os
+from django.core.exceptions import ValidationError
+
+ALLOWED_EXTENSIONS = ['.docx', '.pdf']
+
+
+def validate_extension(value):
+    split_ext = os.path.splitext(value.name)
+    print('split_ext', split_ext)
+    if len(split_ext) > 1:
+        ext = split_ext[1]
+        if not ext.lower() in ALLOWED_EXTENSIONS:
+            raise ValidationError(f'not allowed file, valid extensions: {ALLOWED_EXTENSIONS}')
 
 
 class Category(models.Model):
     name = models.CharField(max_length=300)
+    category_desc = models.FileField(upload_to='desc_files',
+                                     validators=[validate_extension],
+                                     null=True, blank=True)
 
     class Meta:
         verbose_name = 'Category'
--- week12/onlineShop/onlineShop/main/serializers.py
+++ week12/onlineShop/onlineShop/main/serializers.py
@@ -5,6 +5,7 @@ from onlineShop.main.models import Category
 class CategorySerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(required=True)
+    category_desc = serializers.FileField(required=True)
 
     def create(self, validated_data):
         category = Category(**validated_data)
@@ -13,5 +14,16 @@ class CategorySerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
+        instance.category_desc = validated_data.get('category_desc', instance.category_desc)
         instance.save()
         return instance
+
+    def validate(self, attrs):
+        return attrs
+
+# class CategorySerializer(serializers.ModelSerializer):
+#     category_desc = serializers.FileField()
+#
+#     class Meta:
+#         modal = Category
+#         fields = ('id', 'name', 'category_desc')
